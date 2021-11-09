7163249cd9dc8f98a7cb8f878642828eb45375af
wissen-satya <60770284+wissen-satya@users.noreply.github.com>
2021-11-09 15:15:51 +0530

    Update webapp.tf

--- webapp.tf
+++ webapp.tf
@@ -1,13 +1,4 @@
 
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "=2.71.0"
-    }
-  }
-}
-
 
 resource "azurerm_app_service_plan" "appserviceplan" {
   name                = "appserviceplan"
