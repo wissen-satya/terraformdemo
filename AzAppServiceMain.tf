67faef41264160e3beabba1ce9145b7c483392d1
wissen-satya <60770284+wissen-satya@users.noreply.github.com>
2021-11-09 14:58:35 +0530

    Update AzAppServiceMain.tf

--- AzAppServiceMain.tf
+++ AzAppServiceMain.tf
@@ -1,17 +1,4 @@
 /*
-* Provider block defines which provider they require
-*/
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "2.71.0"
-    }
-  }
-}
-
-
-/*
 * Resource Group
 */
 resource "azurerm_resource_group" "RG" {
