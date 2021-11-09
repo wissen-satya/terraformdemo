36618e670d4326449482387261eeef194cbcad86
wissen-satya <60770284+wissen-satya@users.noreply.github.com>
2021-11-09 15:10:16 +0530

    Update webapp.tf

--- webapp.tf
+++ webapp.tf
@@ -1,29 +1,3 @@
-# We strongly recommend using the required_providers block to set the
-# Azure Provider source and version being used
-#Reference https://docs.microsoft.com/en-us/azure/app-service/scripts/terraform-secure-backend-frontend  
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "=2.71.0"
-    }
-  }
-}
-
-# Configure the Microsoft Azure Provider
-provider "azurerm" {
-  features {}
-
-  # More information on the authentication methods supported by
-  # the AzureRM Provider can be found here:
-  # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs
-
-  subscription_id = "65911ece-b772-4e16-ab25-fc95982846dc"
-   client_id       = "37f075a8-9437-4317-84e7-8106b3c2c6a3"
-   client_secret   = "ui8lG0EanXUt.M3ZM-T8P8krICoaEk.RS6"
-   tenant_id       = "8a38d5c9-ff2f-479e-8637-d73f6241a4f0"
-}
-
 
 
 
